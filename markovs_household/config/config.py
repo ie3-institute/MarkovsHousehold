678e83a4763f074b34703d81a5d69bf3cf9392d0
t-ober <63147366+t-ober@users.noreply.github.com>
2021-10-08 10:19:18 +0200

    add test

--- markovs_household/config/config.py
+++ markovs_household/config/config.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 import yaml
 
-@dataclass
+
+@dataclass(frozen=True)
 class Configuration:
     consider_load_shifting: bool
     simulate_typical_days: bool
@@ -9,7 +10,7 @@ class Configuration:
     @staticmethod
     def parse_config(path):
         with open(path, "r") as ymlfile:
-            config = yaml.load(ymlfile)
+            config = yaml.load(ymlfile, Loader=yaml.FullLoader)
         return Configuration(
             config["consider_load_shifting"],
             config["simulate_typical_days"]
