eeb9c66c6e726fd7ef2e409a60a7390199c60156
t-ober <63147366+t-ober@users.noreply.github.com>
2021-11-22 08:54:23 +0100

    use random seed

--- tests/common/test_data.py
+++ tests/common/test_data.py
@@ -11,6 +11,7 @@ SWITCH_ON_PROBABILITY_KEYS = [SwitchOnProbabilityKey(season, day_type, quarterly
                               for day_type in DayType
                               for quarterly_hour_of_day in range(96)]
 
+random.seed(42)
 RANDOM_SWITCH_ON_PROBABILITIES = SwitchOnProbabilities({key: random.random() for key in SWITCH_ON_PROBABILITY_KEYS})
 LOAD_PROFILE_STOVE = TimeSeries({quarterly_hour: 700 for quarterly_hour in range(4)})
 STOVE = ApplianceLoadProfile(type=ApplianceType.STOVE, switch_on_probabilities=RANDOM_SWITCH_ON_PROBABILITIES, profile=LOAD_PROFILE_STOVE)
