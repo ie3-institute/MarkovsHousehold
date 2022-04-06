1786672385715b7bd15a363aa11ef239ecb98527
smnioffe <nils.offermann@tu-dortmund.de>
2022-04-06 16:16:11 +0200

    add usage probabilities class

--- markovs_household/data/usage_probabilities.py
+++ markovs_household/data/usage_probabilities.py
@@ -1,18 +1,31 @@
 import pandas as pd
-from markovs_household.data.appliance import ApplianceType
+from typing import Dict
+from markovs_household.data.appliance import ApplianceCategory
+
 
 class UsageProbabilities:
+    __probabilities: Dict[ApplianceCategory, float]
 
-# __init__ statt methode sinnvoll?
-# wofuer sind unten die unterstriche bei self.__probabilities
-# muessen/sollten wir in der klasse zu Beginn auch Klassenvariablen definieren?
 
     @classmethod
     def read_usage_probabilities(cls, config):
-        df_usage_probabilities = pd.read_csv(config.usage_probabilities_path)
-        for data in ApplianceType:
-            if not(df_usage_probabilities.ApplianceType.str.contains(data.value.replace(' ', '_')).any()):
-                raise ValueError("ApplianceType not found")
+        df_usage_probabilities = pd.read_csv(config.usage_probabilities_file, sep=';')
+        for data in ApplianceCategory:
+            if not (df_usage_probabilities["ApplianceType"].str.contains(data.value.replace(' ', '_')).any()):
+                raise ValueError(data)
 
-        cls.__probabilities = df_usage_probabilities.to_dict
+        df_usage_probabilities = df_usage_probabilities.set_index("ApplianceType")
 
+        return df_usage_probabilities.to_dict()
+
+    @classmethod
+    def get_usage_probability(cls, cat: ApplianceCategory, config):
+        cls.__probabilities = cls.read_usage_probabilities(config)
+
+        return cls.__probabilities["UsageProbability"][cat]
+
+
+from markovs_household.config.config import Configuration
+
+config = Configuration.parse_config(r"tests\input\config.yaml")
+print(UsageProbabilities.get_usage_probability('dryer', config))
